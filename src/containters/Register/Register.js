10d14204eae905425676d4d6c3d98b0a21a8a0ab
dijanal <dijanans5@hotmail.com>
2018-04-17 01:55:16 +0200

    Update Register

--- src/containters/Register/Register.js
+++ src/containters/Register/Register.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import './Register.css';
 
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
+
 class Register extends Component {
     render(){
         return(
-            <div>
-                <p>Register</p>
-            </div>
+          <div className='login'>
+          <Input type='email'
+                 placeholder='email..'
+          />
+          <Input type='password'
+                 placeholder='password...'
+          />
+          <Input type='password'
+                 placeholder='password...'
+          />
+          <Button value='Log In'/>
+          </div>
         )
     }
 }
